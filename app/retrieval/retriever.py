6d5857f26730e15d22f9ead80fbf21303b383f9d
Rahul74-72 <72rahuljat@gmail.com>
2026-08-12 19:54:19 +0530

    feat: add memory retrieval

---
+++ app/retrieval/retriever.py
@@ -0,0 +1,34 @@
+from sqlalchemy import select
+
+from app.database.database import SessionLocal
+from app.database.models import Memory
+
+
+class MemoryRetriever:
+    def __init__(self):
+        self.session = SessionLocal()
+
+    def search(self, question):
+        words = question.lower().split()
+        results = []
+
+        stmt = select(Memory).where(Memory.active.is_(True))
+        memories = self.session.execute(stmt).scalars().all()
+
+        for memory in memories:
+            searchable_text = (
+                f"{memory.relation} {memory.value} {memory.category}"
+            ).lower()
+
+            for word in words:
+                if len(word) < 4:
+                    continue
+                if word in searchable_text:
+                    if memory not in results:
+                        results.append(memory)
+                    break
+
+        return results
+
+    def close(self):
+        self.session.close()
