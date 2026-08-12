1e73a5569b55057710d92919839704073a43b5c2
Rahul74-72 <72rahuljat@gmail.com>
2026-08-12 19:56:51 +0530

    Remove test scripts

--- test_memory.py
+++
@@ -1,9 +0,0 @@
-from app.memory.memory import MemoryManager
-
-memory = MemoryManager()
-
-print("\nStored Conversations:\n")
-for msg in memory.get_all_messages():
-    print(f"{msg.id} | {msg.speaker} | {msg.message}")
-
-memory.close()
